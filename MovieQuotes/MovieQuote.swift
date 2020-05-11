67c3d702db59c204b989cbf84ad0595d6e4f61fa
Yifei Li <liy12@rose-hulman.edu>
2020-05-11 05:07:59 -0400

    show movie quote

---
+++ MovieQuotes/MovieQuote.swift
@@ -0,0 +1,19 @@
+//
+//  MovieQuote.swift
+//  MovieQuotes
+//
+//  Created by Yifei Li on 5/11/20.
+//  Copyright © 2020 Yifei Li. All rights reserved.
+//
+
+import Foundation
+
+class MovieQuote{
+    var quote: String
+    var moive: String
+    
+    init(quote: String, movie: String){
+        self.quote = quote
+        self.moive = movie
+    }
+}
--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class MovieQuotesTableViewController:UITableViewController {
     let movieQuoteCellIndentifier = "MovieQuoteCell"
-    var names = ["Dave", "Li", "Gao"]
+//    var names = ["Dave", "Li", "Gao"]
+    var movieQuotes = [MovieQuote]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        movieQuotes.append(MovieQuote(quote: "I'll be back", movie: "The Terminator"))
+        movieQuotes.append(MovieQuote(quote: "Yo Adrain!", movie: "Rocky"))
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return movieQuotes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: movieQuoteCellIndentifier, for: indexPath)
         // Configure the cell
-        cell.textLabel?.text = names[indexPath.row]
+//        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = movieQuotes[indexPath.row].quote
+        cell.detailTextLabel?.text = movieQuotes[indexPath.row].moive
         return cell
     }
     
