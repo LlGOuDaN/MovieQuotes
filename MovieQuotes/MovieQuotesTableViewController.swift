636c0416a0db19de47d7693d71a0f86618da5e97
Yifei Li <liy12@rose-hulman.edu>
2020-05-11 05:14:39 -0400

    about to add dialog

--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -15,12 +15,18 @@ class MovieQuotesTableViewController:UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(showAddQuoteDialog))
         movieQuotes.append(MovieQuote(quote: "I'll be back", movie: "The Terminator"))
         movieQuotes.append(MovieQuote(quote: "Yo Adrain!", movie: "Rocky"))
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movieQuotes.count
+    }
+    
+    @objc func showAddQuoteDialog(){
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
